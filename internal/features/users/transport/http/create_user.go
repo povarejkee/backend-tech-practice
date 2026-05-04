598c6d7c35e66d185e21e059c3cf6efb60481a89
Nagibator777 <i.popov@opencorepay.com>
2026-05-04 11:10:58 +0300

    feat(app): added validation pack, started decode-and-validate req

--- internal/features/users/transport/http/create_user.go
+++ internal/features/users/transport/http/create_user.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 
 	core_logger "github.com/povarejkee/backend-tech-practice/internal/core/logger"
+	core_http_request "github.com/povarejkee/backend-tech-practice/internal/core/transport/http/request"
 )
 
 type CreateUserRequest struct {
-	FullName    string  `json:"full_name"`
-	PhoneNumber *string `json:"phone_number"`
+	FullName    string  `json:"full_name" validate:"required,min=3,max=100"`
+	PhoneNumber *string `json:"phone_number" validate:"omitempty,min=10,max=15,startswith=+"`
 }
 
 type CreateUserResponse struct {
@@ -26,6 +27,10 @@ func (h *UsersHTTPHandler) CreateUser(rw http.ResponseWriter, r *http.Request) {
 	log.Debug("invoce CreateUser handler")
 
 	var req CreateUserRequest
+	if err := core_http_request.DecodeAndValidate(r, &req); err != nil {
+		// todo
+	}
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		// todo: handle error
 	}
